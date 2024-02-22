9fdae49625a24acd215ce85eeb48dbf11efbc85d
Ririena <nerllysurina@gmail.com>
2024-02-22 11:10:13 +0700

    Add Api Controller -> Vote

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -18,6 +18,9 @@ class VoteController extends Controller
             'created_at',
             'updated_at',
         ]);
-
+    }
+    public function getData()
+    {
+        echo "Hello Vote";
     }
 }
